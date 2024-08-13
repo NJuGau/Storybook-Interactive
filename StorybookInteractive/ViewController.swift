90ccff00944c052761ff4879afc36ceab6854c9c
NJuGau <nathanael.gauthama@binus.ac.id>
2024-08-13 09:44:22 +0700

    Initial Project Inject

---
+++ StorybookInteractive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StorybookInteractive
+//
+//  Created by Nathanael Juan Gauthama on 13/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
