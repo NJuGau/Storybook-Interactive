ff411b194e410bb4ae4cb6cdf0e571e6d117fbcf
Elsavira <elsavira177@gmail.com>
2024-08-15 17:37:08 +0700

    feat: slicing background

--- StorybookInteractive/Presentation/Views/LearningGuidanceViewController.swift
+++ StorybookInteractive/Presentation/Views/LearningGuidanceViewController.swift
@@ -13,21 +13,38 @@ class LearningGuidanceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(tutorialLabel)
+        
+        view.addSubview(backgroundView)
+        backgroundView.addSubview(tutorialLabel)
+        
+        NSLayoutConstraint.activate([
+            backgroundView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            backgroundView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            backgroundView.widthAnchor.constraint(equalToConstant: 900),
+            backgroundView.heightAnchor.constraint(equalToConstant: 650)
+        ])
         
         NSLayoutConstraint.activate([
             tutorialLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            tutorialLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -300)
+            tutorialLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -250)
         
         ])
     }
+    
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .lightGray
+        view.layer.cornerRadius = 10
+        return view
+    }()
     
     private let tutorialLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Tutorial"
         label.textColor = .black
-        label.font = UIFont.systemFont(ofSize: 18)
+        label.font = UIFont.systemFont(ofSize: 64)
         
         return label
     }()
