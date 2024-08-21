b22dcce26617f9b9c5ed1900c0904ef46e6b69de
Elsavira <elsavira177@gmail.com>
2024-08-21 10:31:39 +0700

    feat: slicing background

--- StorybookInteractive/Presentation/Views/LetsReadViewController.swift
+++ StorybookInteractive/Presentation/Views/LetsReadViewController.swift
@@ -14,54 +14,47 @@ class LetsReadViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        view.addSubview(backgroundImageView)
         view.addSubview(bookTitle)
-        view.addSubview(startReadingButton)
-        view.addSubview(homeButton)
+        view.addSubview(appLogo)
         
         NSLayoutConstraint.activate([
             bookTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            bookTitle.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -240),
+            bookTitle.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -190),
             
-            startReadingButton.topAnchor.constraint(equalTo: bookTitle.bottomAnchor, constant: 450),
-            startReadingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            startReadingButton.widthAnchor.constraint(equalToConstant: 270),
-            startReadingButton.heightAnchor.constraint(equalToConstant: 64),
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
-            homeButton.widthAnchor.constraint(equalToConstant: 69),
-            homeButton.heightAnchor.constraint(equalToConstant: 69),
-            homeButton.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -374),
-            homeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32)
+            appLogo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            appLogo.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -330)
+            
         ])
     }
     
-    private let bookTitle: UILabel = {
-        let bookTitle = UILabel()
+    private let bookTitle: UIImageView = {
+        let bookTitle = UIImageView()
         bookTitle.translatesAutoresizingMaskIntoConstraints = false
-        bookTitle.text = "Judul Buku"
-        bookTitle.font = UIFont.boldSystemFont(ofSize: 74)
+        bookTitle.image = UIImage(named: "Title")
         return bookTitle
     }()
     
-    private let startReadingButton: UIButton = {
-        let startReadingButton = UIButton()
-        startReadingButton.translatesAutoresizingMaskIntoConstraints = false
-        startReadingButton.backgroundColor = UIColor(red: 0.53, green: 0.68, blue: 0.58, alpha: 1)
-        startReadingButton.layer.cornerRadius = 16
-        startReadingButton.setTitle("Mulai Membaca", for: .normal)
-        startReadingButton.titleLabel?.font = .boldSystemFont(ofSize: 28)
-        return startReadingButton
+    private let backgroundImageView: UIImageView = {
+        let backgroundImageView = UIImageView()
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.image = UIImage(named: "Cover")
+        backgroundImageView.contentMode = .scaleAspectFill
+        return backgroundImageView
     }()
     
-    private let homeButton: UIButton = {
-        let homeButton = UIButton()
-        homeButton.translatesAutoresizingMaskIntoConstraints = false
-        homeButton.backgroundColor = UIColor(red: 0.93, green: 0.96, blue: 0.88, alpha: 1)
-        homeButton.layer.cornerRadius = 69 / 2
-        homeButton.clipsToBounds = true
-        let buttonImage = UIImage(named: "home")
-        homeButton.setImage(buttonImage, for: .normal)
-        return homeButton
+    private let appLogo: UIImageView = {
+        let appLogo = UIImageView()
+        appLogo.translatesAutoresizingMaskIntoConstraints = false
+        appLogo.image = UIImage(named: "Vocaloom")
+        return appLogo
     }()
+
     
 }
 
