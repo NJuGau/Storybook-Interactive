05a161387ca5ed41815032aacd641647b27c8efc
Doni Pebruwatoro <85363247+pebruwantoro@users.noreply.github.com>
2024-08-21 11:30:17 +0700

    fixing-unit-test (#11)

--- StorybookInteractiveTests/StorybookInteractiveTests.swift
+++ StorybookInteractiveTests/StorybookInteractiveTests.swift
@@ -10,13 +10,28 @@ import XCTest
 
 final class StorybookInteractiveTests: XCTestCase {
 
-    private var useCase: StorybookUsecase!
-    private var repository: StorybookRepository!
+    private var bookUsecase: BookUsecase!
+    private var storyUsecase: StoryUsecase!
+    private var backgroundUsecase: BackgroundUsecase!
+    private var objectImageUsecase: ObjectImageUsecase!
+    
+    private var bookRepository: BookRepository!
+    private var storyRepository: StoryRepository!
+    private var backgroundRepository: BackgroundRepository!
+    private var objectImageRepository: ObjectImageRepository!
+    
     
     override func setUp() {
         super.setUp()
-        repository = JSONStorybookRepository()
-        useCase = StorybookUsecase(storyRepository: repository)
+        bookRepository = JSONBookRepository()
+        storyRepository = JSONStoryRepository()
+        backgroundRepository = JSONBackgroundRepository()
+        objectImageRepository = JSONObjectImageRepository()
+        
+        bookUsecase = BookUsecase(bookRepository: bookRepository)
+        storyUsecase = StoryUsecase(storyRepository: storyRepository)
+        backgroundUsecase = BackgroundUsecase(backgroundRepository: backgroundRepository)
+        objectImageUsecase = ObjectImageUsecase(objectImageRepository: objectImageRepository)
     }
     
     override func tearDown() {
@@ -35,7 +50,7 @@ final class StorybookInteractiveTests: XCTestCase {
             return
         }
         
-        let bookList = useCase.fetchListBook()
+        let bookList = bookUsecase.fetchListBook()
         
         XCTAssertFalse(bookList.0.isEmpty, "BookList returns an empty array")
     }
@@ -52,76 +67,8 @@ final class StorybookInteractiveTests: XCTestCase {
             return
         }
         
-        let book = useCase.fetchBookById(id: 1)
+        let book = bookUsecase.fetchBookById(req: BookRequest(id: "37bff686-7d09-4e53-aa90-fb465da131b5"))
         
-        XCTAssertFalse(book.0, "Book returns an empty array")
+        XCTAssert((book.0 != nil), "Book returns an empty array")
     }
 }
-
-//
-//  PukPuk_Tests.swift
-//  PukPuk-Tests
-//
-//  Created by Muhammad Rasyad Caesarardhi on 14/08/24.
-//
-
-//@testable import PukPuk
-//import XCTest
-//
-//final class PukPuk_Tests: XCTestCase {
-//    private var useCase: HomeUseCase!
-//    private var repository: HomeDefaultRepository!
-//    private var dataSource: HomeLocalDataSource!
-//    
-//    override func setUp() {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//        super.setUp()
-//        dataSource = HomeLocalDataSource()
-//        repository = HomeDefaultRepository(homeLocalDataSource: dataSource)
-//        useCase = HomeUseCase(homeRepository: repository)
-//    }
-//    
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//        super.tearDown()
-//    }
-//    
-//    func testCryClassificationIndicatesNotCry() async throws {
-//        // Use a valid URL for a test file where "not_cry" is expected
-//        guard let url = Bundle.main.url(forResource: "not_cry", withExtension: "wav") else {
-//            XCTFail("URL for the resource is nil.")
-//            return
-//        }
-//
-//        do {
-//            // Call the function that is expected to throw an error
-//            let _ = try await useCase.getModelResult(url: url)
-//            XCTFail("Expected error, but got response")
-//        } catch let error as CryClassificationError {
-//            XCTAssertEqual(error, .notCryDetected)
-//        } catch {
-//            XCTFail("Unexpected error: \(error)")
-//        }
-//    }
-//        
-//    func testCryClassificationSuccess() async throws {
-//        // Use a valid URL for a test file where "cry" is expected
-//        guard let url = Bundle.main.url(forResource: "542c", withExtension: "wav") else {
-//            XCTFail("URL for the resource is nil.")
-//            return
-//        }
-//
-//        do {
-//            // Call the function to get classification results
-//           
-//            let results = try await useCase.getModelResult(url: url)
-//                
-//            // Check that results are not empty
-//            XCTAssertFalse(results.isEmpty, "Results should not be empty")
-//            // Additional assertions for specific results can be added here
-//                
-//        } catch {
-//            XCTFail("Expected success, but got error: \(error)")
-//        }
-//    }
-//}
